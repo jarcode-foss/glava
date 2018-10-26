9699d27ab847d2a5575e77f6b404df3087f63710
Aaahh Ahh <woohoomoo2u@gmail.com>
2018-10-25 22:54:01 -0400

    Defaults before user config for Bars module

--- shaders/bars.glsl
+++ shaders/bars.glsl
@@ -1,32 +1,74 @@
+#include ":bars-user.glsl"
 
 /* Center line thickness (pixels) */
-#define C_LINE 1
+#ifndef C_LINE
+    #define C_LINE 1
+#endif
 
 /* Width (in pixels) of each bar */
-#define BAR_WIDTH 4
+#ifndef BAR_WIDTH
+    #define BAR_WIDTH 4
+#endif
+
 /* Width (in pixels) of each bar gap */
-#define BAR_GAP 2
+#ifndef BAR_GAP
+    #define BAR_GAP 2
+#endif
+
 /* Outline color */
-#define BAR_OUTLINE #262626
+#ifndef BAR_OUTLINE
+    #define BAR_OUTLINE #262626
+#endif
+
 /* Outline width (in pixels, set to 0 to disable outline drawing) */
-#define BAR_OUTLINE_WIDTH 0
+#ifndef BAR_OUTLINE_WIDTH
+    #define BAR_OUTLINE_WIDTH 0
+#endif
+
 /* Amplify magnitude of the results each bar displays */
-#define AMPLIFY 300
+#ifndef AMPLIFY
+    #define AMPLIFY 300
+#endif
+
 /* Alpha channel for bars color */
-#define ALPHA 0.7
+#ifndef ALPHA
+    #define ALPHA 0.7
+#endif
+
 /* How strong the gradient changes */
-#define GRADIENT_POWER 60
+#ifndef GRADIENT_POWER
+    #define GRADIENT_POWER 60
+#endif
+
 /* Bar color changes with height */
-#define GRADIENT (d / GRADIENT_POWER + 1)
+#ifndef GRADIENT
+    #define GRADIENT (d / GRADIENT_POWER + 1)
+#endif
+
 /* Bar color */
-#define COLOR (#3366b2 * GRADIENT * ALPHA)
+#ifndef COLOR
+    #define COLOR (#3366b2 * GRADIENT * ALPHA)
+#endif
+
 /* Direction that the bars are facing, 0 for inward, 1 for outward */
-#define DIRECTION 0
+#ifndef BAR_WIDTH)
+    #define DIRECTION 0
+#endif
+
 /* Whether to switch left/right audio buffers */
-#define INVERT 0
+#ifndef INVERT
+    #define INVERT 0
+#endif
+
 /* Whether to flip the output vertically */
-#define FLIP 0
+#ifndef FLIP
+    #define FLIP 0
+#endif
+
+
 /* Whether to mirror output along `Y = X`, causing output to render on the left side of the window */
 /* Use with `FLIP 1` to render on the right side */
-#define MIRROR_YX 0
+#ifndef MIRROR_YX
+    #define MIRROR_YX 0
+#endif
 
