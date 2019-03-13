7ff896d6d834e8c80392ca535c5528661f0c9c68
Robin B <robin@broda.me>
2019-03-13 23:58:48 +0100

    wave: Use STDIN as base color when available

--- shaders/wave.glsl
+++ shaders/wave.glsl
@@ -5,7 +5,11 @@
 #define MAX_THICKNESS 6
 
 /* base color to use, distance from center will multiply the RGB components */
+#if USE_STDIN == 1
+#define BASE_COLOR STDIN
+#else
 #define BASE_COLOR vec4(0.7, 0.2, 0.45, 1)
+#endif
 
 /* amplitude */
 #define AMPLIFY 500
