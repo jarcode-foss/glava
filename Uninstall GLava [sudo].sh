378b6f0ff2af6fb5beb6bcecac699a806de75329
Adamecki <99909156+adamecki@users.noreply.github.com>
2023-01-26 13:31:24 +0100

    Included an uninstaller

---
+++ Uninstall GLava [sudo].sh
@@ -0,0 +1,7 @@
+rm -rf /etc/xdg/glava
+rm /usr/bin/glava
+rm /usr/local/bin/glava
+rm /usr/lib/x86_64-linux-gnu/libglava.so
+rm /usr/bin/glava
+rm /usr/local/lib/x86_64-linux-gnu/libglava.so
+rm -rf ~/.config/glava
